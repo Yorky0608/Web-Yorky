8d27da4f7f9e36b2d517aca3eb492febb0012716
Yorky0608 <>
2025-05-05 12:52:30 -0400

    .

--- york/urls.py
+++ york/urls.py
@@ -21,13 +21,13 @@ urlpatterns = [
     path('about_me/', views.about_me, name='about_me'),
     path('interests/', views.interests, name='interests'),
     path('blogs/', views.blogs, name='blogs'),
-    # Detail page for a single topic.
+    # Detail page for a single blog.
     path('blogs/<int:blog_id>/', views.posts, name='posts'),
-    # Page for adding a new topic.
+    # Page for adding a new blog.
     path('new_blog/', views.new_blog, name='new_blog'),
-    # Page for adding a new entry.
+    # Page for adding a new post.
     path('new_post/<int:blog_id>/', views.new_post, name='new_post'),
-    # Page for editing an entry.
+    # Page for editing an post.
     path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
     ]
 
--- york/views.py
+++ york/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Question, Response, Video, Blog, Post
@@ -10,17 +10,14 @@ import pandas as pd
 
 # Create your views here.
 def index(request):
-    """The home page for Learning Log."""
     return render(request, 'york/index.html')
 
 def questions(request):
-    """Show all topics."""
     questions = Question.objects.order_by('text')
     context = {'questions': questions}
     return render(request, 'york/questions.html', context)
 
 def responses(request, question_id):
-    """Show a single topic and all its entries."""
     question = Question.objects.get(id=question_id)
     responses = question.response_set.order_by('-date_added')
     context = {'question': question, 'responses': responses}
@@ -31,12 +28,9 @@ def responses(request, question_id):
 
 @login_required
 def new_question(request):
-    """Add a new topic."""
     if request.method != 'POST':
-        # No data submitted; create a blank form.
         form = QuestionForm()
     else:
-        # POST data submitted; process data.
         form = QuestionForm(data=request.POST)
         if form.is_valid():
             new_topic = form.save(commit=False)
@@ -44,20 +38,16 @@ def new_question(request):
             new_topic.save()
             return redirect('york:questions')
 
-    # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'york/new_question.html', context)
 
 @login_required
 def new_response(request, question_id):
-    """Add a new entry for a particular topic."""
     question = Question.objects.get(id=question_id)
 
     if request.method != 'POST':
-    # No data submitted; create a blank form.
         form = ResponseForm()
     else:
-        # POST data submitted; process data.
         form = ResponseForm(data=request.POST)
         if form.is_valid():
             new_entry = form.save(commit=False)
@@ -66,13 +56,11 @@ def new_response(request, question_id):
             new_entry.save()
             return redirect('york:responses', question_id=question_id)
 
-        # Display a blank or invalid form.
     context = {'question': question, 'form': form}
     return render(request, 'york/new_response.html', context)
 
 @login_required
 def edit_response(request, response_id):
-    """Edit an existing entry."""
     response = Response.objects.get(id=response_id)
     question = response.question
 
@@ -80,10 +68,8 @@ def edit_response(request, response_id):
         return redirect('york:index')
     else:
         if request.method != 'POST':
-            # Initial request; pre-fill form with the current entry.
             form = ResponseForm(instance=question)
         else:
-            # POST data submitted; process data.
             form = ResponseForm(instance=question, data=request.POST)
             if form.is_valid():
                 form.save
@@ -94,13 +80,11 @@ def edit_response(request, response_id):
 
 
 def blogs(request):
-    """Show all topics."""
     blogs = Blog.objects.order_by('text')
     context = {'blogs': blogs}
     return render(request, 'york/blogs.html', context)
 
 def posts(request, blog_id):
-    """Show a single topic and all its entries."""
     blog = Blog.objects.get(id=blog_id)
     posts = blog.post_set.order_by('-date_added')
     context = {'blog': blog, 'posts': posts}
@@ -111,12 +95,9 @@ def posts(request, blog_id):
 
 @login_required
 def new_blog(request):
-    """Add a new topic."""
     if request.method != 'POST':
-        # No data submitted; create a blank form.
         form = BlogForm()
     else:
-        # POST data submitted; process data.
         form = BlogForm(data=request.POST)
         if form.is_valid():
             new_topic = form.save(commit=False)
@@ -124,20 +105,16 @@ def new_blog(request):
             new_topic.save()
             return redirect('york:blogs')
 
-    # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'york/new_blog.html', context)
 
 @login_required
 def new_post(request, blog_id):
-    """Add a new entry for a particular topic."""
     blog = Blog.objects.get(id=blog_id)
 
     if request.method != 'POST':
-    # No data submitted; create a blank form.
         form = PostForm()
     else:
-        # POST data submitted; process data.
         form = PostForm(data=request.POST)
         if form.is_valid():
             new_entry = form.save(commit=False)
@@ -146,13 +123,11 @@ def new_post(request, blog_id):
             new_entry.save()
             return redirect('york:posts', blog_id=blog_id)
 
-        # Display a blank or invalid form.
     context = {'blog': blog, 'form': form}
     return render(request, 'york/new_post.html', context)
 
 @login_required
 def edit_post(request, post_id):
-    """Edit an existing entry."""
     post = Post.objects.get(id=post_id)
     blog = post.blog
 
@@ -160,7 +135,6 @@ def edit_post(request, post_id):
         return redirect('york:index')
     else:
         if request.method != 'POST':
-            # Initial request; pre-fill form with the current entry.
             form = PostForm(instance=post)
         else:
             # POST data submitted; process data.
